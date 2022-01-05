b46f6d8d8779cb3b01304543f749f0c1c4829a0c
Mike Kang <mike.kang@felix.net>
2022-01-06 09:45:36 +1000

    first commit

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    testEnvironment: 'jest-environment-jsdom',
+    setupFilesAfterEnv: ['./src/setupTests.ts']
+       // ... other options ...
+}
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div className="bg-black">
+    <h1 className="text-white mt-">learn react</h1>
+  </div>
+);
+
+export default App;
